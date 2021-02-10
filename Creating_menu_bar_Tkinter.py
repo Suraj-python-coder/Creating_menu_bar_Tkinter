f312da2cd61c155bb4d14ff8ca58380989e4bcd4
Suraj Yelekar <srjyelekar@gmail.com>
2021-02-10 14:34:30 +0530

    Menu bar commit

---
+++ Creating_menu_bar_Tkinter.py
@@ -0,0 +1,43 @@
+# creating menu bar:
+# ====================
+
+
+import tkinter as tk       
+from tkinter import ttk  
+win = tk.Tk() 
+win.title('MENU_BAR')
+
+
+def func():
+    print('func called')
+
+# Menu --> this Menu class present in tkinter
+# **************************SIMPLE MENUBAR*****************************
+# menubar = tk.Menu(win)
+# menubar.add_command(label='Save', command=func)
+# menubar.add_command(label='Save As', command=func)
+# menubar.add_command(label='Copy', command=func)
+# menubar.add_command(label='Pest', command=func)
+
+main_menu = tk.Menu(win)
+# file menu
+file_menu = tk.Menu(main_menu, tearoff=0)
+file_menu.add_command(label='New File', command=func)
+file_menu.add_command(label='New Window', command=func)
+file_menu.add_separator()
+file_menu.add_command(label='Save File', command=func)
+
+# edit menu
+edit_menu = tk.Menu(main_menu, tearoff=0)
+edit_menu.add_command(label='Undo', command=func)
+edit_menu.add_separator()
+edit_menu.add_command(label='Redo', command=func)
+
+
+main_menu.add_cascade(label='File',menu=file_menu)
+main_menu.add_cascade(label='Edit',menu=edit_menu)
+
+
+win.config(menu=main_menu)
+
+win.mainloop()
